b95fcc69a8618ff9795afbf8329bfd7c4de935f2
KranthiKG <97874149+KranthiKG@users.noreply.github.com>
2023-02-17 16:19:05 +0530

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -4,9 +4,7 @@ def dockerBuild(String repository){
   """
 }
 
-def dockerBuild1(){
-  sh 'docker build -t kranthikg/sharedlibimage .'
-}
+
 
 def dockerPush(String repository){
   sh "docker push ${repository}"
@@ -15,10 +13,23 @@ def dockerPush(String repository){
 def dockerLogin(){
  withCredentials([string(credentialsId: 'dockerhubpwd', variable: 'dockerhubpwd')]) {
    sh """
-   docker login -u kranthikg -p ${dockerhubpwd}
+   docker login --username="kranthikg" --password="${dockerhubpwd}"
    """
  }
 }
 def dockerPull(String repository){
   sh "docker pull ${repository}"
+}
+
+def dockerBuildPush(String dockerHubCredentialsId, String dockerFilePath, String dockerImageName, String dockerImageTag) {
+    try {
+        def docker = new DockerClient('unix:///var/run/docker.sock')
+        docker.withRegistry("https://index.docker.io/v1/", dockerhubpwd) {
+            def dockerImage = docker.build("${dockerImageName}:${dockerImageTag}", dockerFilePath)
+            dockerImage.push()
+        }
+    } catch (e) {
+        println("Failed to build and push Docker image: ${e}")
+        throw e
+    }
 }
