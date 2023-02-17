c805224f46717e86f440721b0e764595398e6c69
KranthiKG <97874149+KranthiKG@users.noreply.github.com>
2023-02-17 14:12:17 +0530

    Update docker.groovy

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -10,6 +10,12 @@ def dockerPush(String repository){
   sh "docker push ${repository}"
 }
 
+def dockerPush1(string repository)
+ withCredentials([string(credentialsId: 'dockerhubpwd', variable: 'dockerhubpwd')]) {
+                    sh 'docker login -u kranthikg -p ${dockerhubpwd}'
+}
+sh "docker push ${repository}"
+
 def dockerPull(String repository){
   sh "docker pull ${repository}"
 }
