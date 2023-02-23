3ee225ac02ecd0fdf095ae1bd7fe05593ed6f408
KranthiKG <97874149+KranthiKG@users.noreply.github.com>
2023-02-23 10:09:56 +0530

    Update fetchCode.groovy

--- vars/fetchCode.groovy
+++ vars/fetchCode.groovy
@@ -11,15 +11,15 @@ def fetchCode1(){
   git url: "https://github.com/KranthiKG/Devops-Integration-SharedLib.git"
 }
 
-def updateDeploy(){
-                        sh "cat Deployment.yml"
-                        sh "git config user.email kranthikirang@gmail.com"
-                        sh "git config user.name KranthiKG"
-                        sh "sed -i 's+kranthikg/sharedlibimage.*+${DOCKER_IMAGE_TAG}+g' Deployment.yml"
-                        sh "cat Deployment.yml"
-                        sh "git add ."
-                        sh "git commit -m 'Done by Jenkins Job changemanifest: ${env.BUILD_NUMBER}'"
-                        withCredentials([gitUsernamePassword(credentialsId: 'githubcred', gitToolName: 'Default')]) {
-                        sh "git push https://github.com/KranthiKG/Devops-Integration-SharedLib.git HEAD:main"
+// def updateDeploy(){
+//                         sh "cat Deployment.yml"
+//                         sh "git config user.email kranthikirang@gmail.com"
+//                         sh "git config user.name KranthiKG"
+//                         sh "sed -i 's+kranthikg/sharedlibimage.*+${DOCKER_IMAGE_TAG}+g' Deployment.yml"
+//                         sh "cat Deployment.yml"
+//                         sh "git add ."
+//                         sh "git commit -m 'Done by Jenkins Job changemanifest: ${env.BUILD_NUMBER}'"
+//                         withCredentials([gitUsernamePassword(credentialsId: 'githubcred', gitToolName: 'Default')]) {
+//                         sh "git push https://github.com/KranthiKG/Devops-Integration-SharedLib.git HEAD:main"
   
-}
+// }
