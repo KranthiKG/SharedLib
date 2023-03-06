9fb3715f714d78023dc3d36f8eded5355310f20b
KranthiKG <97874149+KranthiKG@users.noreply.github.com>
2023-03-06 16:46:39 +0530

    Update sonarQube.groovy

--- vars/sonarQube.groovy
+++ vars/sonarQube.groovy
@@ -9,3 +9,17 @@ def sonarAnalysis(String Project_Key, String Project_Name, String Sonar_URL, Str
       -Dsonar.host.url=${Sonar_URL}  \
       -Dsonar.login=${Sonar_login}"
 }
+
+// def call(Map config) {
+//   stage('Sonar') {
+//     withSonarQubeEnv('sonarqube') {
+//       sh "mvn sonar:sonar -Dsonar.projectKey=${config.projectKey} -Dsonar.login=${config.login} -Dsonar.projectVersion=${config.projectVersion} -Dsonar.host.url=${config.hostUrl}"
+//     }
+//   }
+// }
+
+//      stage('Sonar') {
+//             steps {
+//                 sonarQube(projectKey: 'demo-jenkins', login: '4cab7b06e178c4e93a64ac2518a1281a7d480b70', projectVersion: '1.0', hostUrl: 'http://15.206.124.252:9000')
+//               }
+//         }
